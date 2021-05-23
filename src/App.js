07b80892833a1adac90a54ed732b47a13e63d4c7
ally-petitt <allypetitt@gmail.com>
2021-05-23 08:30:32 -0700

    integrated api call and formatted response

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+import Weather from "./components/Weather"
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "weather-icons/css/weather-icons.css"
+
+// make an api call to this link: api.openweathermap.org/data/2.5/weather?q=London,uk
+const API_KEY = "04ae21c0f17814f4a85bbfb1bb2f38d4"
 
 function App() {
+  const [state, setState] = useState({});
+
+  useEffect(async () => {
+    const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=london&appid=${API_KEY}`)
+
+    const res = await apiCall.json();
+
+    console.log(res)
+
+    setState({
+      city: res.name,
+      country: res.sys.country,
+      temp: calcCelcius(res.main.temp),
+      minTemp: calcCelcius(res.main.temp_min),
+      maxTemp: calcCelcius(res.main.temp_max),
+      description: capitalize(res.weather[0].description)
+    })
+
+  }, [])
+
+  const calcCelcius = (temp) => {
+    let celcius = Math.floor(temp - 273.15);
+    return celcius;
+  }
+
+  const capitalize = (string) => {
+    const words = string.split(" ");
+
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+    }
+
+    return words.join(" ");
+}
+
+  console.log(state)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Weather info={state}/>
     </div>
   );
 }
---
+++ src/components/Weather.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Weather({ info }) {
+    return (
+        <div className="container">
+          <div className="cards">
+              <h1>{info.city}, {info.country}</h1>
+              <h5 className="py-4">
+                  <i className="wi wi-day-sunny display-1"></i>
+              </h5>
+              <h1 className="py-2">{info.temp}&deg; C</h1>
+
+              <div className="container w-100 d-flex justify-content-center align-items-center">
+                <h3 className="d-flex">
+                    <div className=" mx-3 mx-md-5 my-3 d-flex flex-column justify-content-center align-items-center">
+                        <span>Low</span>
+                        <span className="px-4">{info.minTemp}&deg; C</span>
+                    </div>
+                    <div className=" mx-3 mx-md-5 my-3 d-flex flex-column justify-content-center align-items-center">
+                        <span>High</span>
+                        <span className="px-4">{info.maxTemp}&deg; C</span>
+                    </div>
+                </h3>
+              </div>
+              <h4 className="py-3">{info.description}</h4>
+            </div> 
+        </div>
+    )
+}
+
+export default Weather
